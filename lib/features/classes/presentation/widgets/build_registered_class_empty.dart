978f1670f47a024a111323a1734aa7bdacb8d2e4
firoos18 <itsfiroos@gmail.com>
2024-11-20 12:32:11 +0700

    fix : adding assets on classes list empty state

--- lib/features/classes/presentation/widgets/build_registered_class_empty.dart
+++ lib/features/classes/presentation/widgets/build_registered_class_empty.dart
@@ -7,12 +7,24 @@ class BuildRegisteredClassEmpty extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(top: 120),
-      child: const Center(
+      child: Center(
         child: SizedBox(
           width: 240,
-          child: Text(
-            'Oops! Tidak ada kelas. Anda belum terdaftar di kelas apapun',
-            textAlign: TextAlign.center,
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/image/empty-assets.png',
+                scale: 2,
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Oops! Tidak ada kelas. Anda belum terdaftar di kelas apapun',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
           ),
         ),
       ),
